2daa4fef7f50ec221c13b9a74f8d6080aabbd30f
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2023-03-29 22:55:41 -0400

    removed if user

--- app.js
+++ app.js
@@ -76,6 +76,12 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+app.use((req, res, next) => {
+  if(!req.session.userId) {
+    return next();
+  } 
+})
+
 // error handler
 // app.use(function(err, req, res, next) {
 //   // set locals, only providing error in development
